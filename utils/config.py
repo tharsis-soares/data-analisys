b626f5b8edab96fce82b813b4fecf7e9ebc953eb
tharsis-soares <tharsissoares@hotmail.com>
2025-03-04 10:14:46 -0300

    supabase-error

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,10 @@
-import os
+import streamlit as st
 from dotenv import load_dotenv
 from supabase import create_client, Client
 
 load_dotenv()  # Carrega variáveis do .env
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+SUPABASE_URL = st.getenv("SUPABASE_URL")
+SUPABASE_KEY = st.getenv("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
